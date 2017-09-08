0ca9950f639519e2227c48c4f267d0875b44aa80
Steven Clarke <stevencl@microsoft.com>
2017-09-08 12:38:30 +0100

    simple change

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {  
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  next(err);;
 });
 
 // error handlers
