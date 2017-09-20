38682521f73a2081f2cee0e39dae6e9ed6133d33
Steven Clarke <stevencl@microsoft.com>
2017-09-20 18:18:06 +0100

    Updated for study

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var router = express.Router();
-//var greeter = require('C:\\utilities\\greetingmodule.js');
+var greeter = require('C:\\utilities\\greetingmodule.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: greeter.greeting('Express') });
 });
 
 
 module.exports = router;
-
-function uncalledFunction() {
-  var f = Math.floor((Math.random() * 100) + 1);
-  return f;
-}
